c07f75a724de001fe3a26c9f8b115f7857e192b6
Bebliuc George <bebliucgeorge@192-168-0-145.rdsnet.ro>
2021-07-07 18:33:38 +0300

    added types

--- src/frontend/components/app/default-dashboard.tsx
+++ src/frontend/components/app/default-dashboard.tsx
@@ -3,16 +3,8 @@ import styled from 'styled-components'
 import {
   Box,
   H2,
-  H5,
-  H4,
   Text,
-  Illustration,
-  IllustrationProps,
-  Button,
 } from '@adminjs/design-system'
-
-import { useTranslation } from '../../hooks'
-
 const pageHeaderPaddingY = 74
 const pageHeaderPaddingX = 250
 
@@ -32,13 +24,6 @@ export const DashboardHeader: React.FC = () => {
   )
 }
 
-type BoxType = {
-  variant: string;
-  title: string;
-  subtitle: string;
-  href: string;
-}
-const boxes = () => [];
 const Card = styled(Box)`
   display: ${({ flex }): string => (flex ? 'flex' : 'block')};
   color: ${({ theme }): string => theme.colors.grey100};
@@ -59,34 +44,6 @@ export const Dashboard: React.FC = () => {
   return (
     <Box>
       <DashboardHeader />
-      <Box
-        mt={['xl', 'xl', '-100px']}
-        mb="xl"
-        mx={[0, 0, 0, 'auto']}
-        px={['default', 'lg', 'xxl', '0']}
-        position="relative"
-        flex
-        flexDirection="row"
-        flexWrap="wrap"
-        width={[1, 1, 1, 1024]}
-      >
-        {boxes().map((box, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <Box key={index} width={[1, 1 / 2, 1 / 2, 1 / 3]} p="lg">
-            <Card as="a">
-              <Text textAlign="center">
-                <Illustration
-                  variant={box.variant as IllustrationProps['variant']}
-                  width={100}
-                  height={70}
-                />
-                <H5 mt="lg">Administration panel</H5>
-                <Text>{box.subtitle}</Text>
-              </Text>
-            </Card>
-          </Box>
-        ))}
-      </Box>
     </Box>
   )
 }
---
+++ types/src/frontend/components/property-type/index.d.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BasePropertyComponentProps } from './base-property-props';
+declare type BasePropertyComponentType = React.FC<BasePropertyComponentProps> & {
+    DefaultType: any;
+    Boolean: any;
+    DateTime: any;
+    RichText: any;
+    Reference: any;
+    TextArea: any;
+    Password: any;
+};
+declare const BasePropertyComponentExtended: BasePropertyComponentType;
+export { BasePropertyComponentExtended as default, BasePropertyComponentExtended as BasePropertyComponent, };
+export * from './base-property-props';
+export * from './utils';
